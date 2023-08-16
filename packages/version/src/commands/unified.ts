85f4ec71085f7eb523c5b3a16a6c474b41fb4ee8
SpiritLing <spiritling@outlook.com>
2023-08-16 15:58:04 +0900

    fix output

--- packages/version/src/commands/unified.ts
+++ packages/version/src/commands/unified.ts
@@ -2,26 +2,45 @@ import { glob } from "glob";
 import cliProgress from "cli-progress";
 import fs from "fs-extra";
 import chalk from "chalk";
+import delay from "../utils/delay";
 
 const unifiedMain = async (version: string, spaces: number) => {
 	const packageFiles = await glob("**/package.json", { ignore: "node_modules/**" });
 	if (packageFiles.length == 0) {
-		console.log("not exist package.json in current dir");
+		console.log(chalk.red("not exist package.json in current dir"));
 		return;
 	}
 	// create a new progress bar instance and use shades_classic theme
 	console.log(chalk.blue("update package.json files: "));
-	const bar = new cliProgress.SingleBar({}, cliProgress.Presets.legacy);
-	bar.start(packageFiles.length, 0);
+	const bar = new cliProgress.SingleBar(
+		{
+			format: "update [{bar}] {percentage}% | {value}/{total} | file: {speed}",
+		},
+		cliProgress.Presets.legacy
+	);
+	bar.start(packageFiles.length, 0, {
+		speed: "N/A",
+	});
 	for (let index = 0; index < packageFiles.length; index++) {
 		const file = packageFiles[index];
+		bar.update(index, {
+			speed: file,
+		});
 		const packageObj = await fs.readJson(file);
 		packageObj.version = version;
 		await fs.writeJson(file, packageObj, { spaces: spaces });
-		bar.increment();
+		bar.update(index + 1, {
+			speed: file,
+		});
 	}
 	bar.stop();
 	console.log(chalk.green(`finish package.json update version count: ${packageFiles.length}`));
+	console.table(
+		packageFiles.map((x) => ({
+			file: x,
+		})),
+		["file"]
+	);
 };
 
 export default unifiedMain;
