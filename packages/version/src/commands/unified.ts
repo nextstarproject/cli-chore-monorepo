5a822fc3d67557007927ae3c2b8b62a0bb74625b
SpiritLing <spiritling@outlook.com>
2023-08-16 14:59:36 +0900

    temp

---
+++ packages/version/src/commands/unified.ts
@@ -0,0 +1,25 @@
+import { glob, globSync, globStream, globStreamSync, Glob } from "glob";
+import cliProgress from "cli-progress";
+import fs from "fs-extra";
+const unifiedMain = async (version: string) => {
+	const packageFiles = await glob("**/package.json", { ignore: "node_modules/**" });
+	if (packageFiles.length == 0) {
+		console.log("not exist package.json in current dir");
+		return;
+	}
+	// create a new progress bar instance and use shades_classic theme
+	// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+	// bar.start(packageFiles.length, 0);
+	for (let index = 0; index < packageFiles.length; index++) {
+		const file = packageFiles[index];
+		const packageObj = await fs.readJson(file);
+		console.log(file);
+		console.log(packageObj.version);
+		packageObj.version = version;
+		await fs.writeJson(file, packageObj);
+		// bar.update(index);
+	}
+	// bar.stop();
+};
+
+export default unifiedMain;
--- packages/version/src/index.ts
+++ packages/version/src/index.ts
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { program } from "commander";
+import unifiedMain from "./commands/unified";
 
 program.name("ns-version").description("CLI unified Update version").version("1.0.0");
 
 program
 	.command("unified")
 	.alias("u")
-	.description("Unified update version")
-	.argument("<string>", "target version")
+	.description("Unified update version \n ignore node_modules")
+	.argument("<version>", "target version")
+	.action(async (str, options) => {
+		console.log(str);
+		console.log(String(options));
+		await unifiedMain(str);
+	});
+
+program
+	.command("update")
+	.alias("up")
+	.description("target package update version")
+	.option("-p, --path <path...>", "target package workspace")
 	.action((str, options) => {
 		console.log(str);
-        console.log(options);
+		console.log(String(options));
+		if (options?.path != undefined) {
+			const workspace = options.path as string[];
+		}
 	});
 
 program.parse();
